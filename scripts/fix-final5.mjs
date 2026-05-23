629ccd165d28d8360054da3b475fc84b10829aee
Mohd Gayasuddin <ladla8602@gmail.com>
2026-05-23 15:03:04 +0530

    fix: template feature remove unused scripts

--- scripts/fix-final5.mjs
+++
@@ -1,5 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs'
-let c = readFileSync('packages/design-editor/src/engine/core/controllers/Guidelines.ts', 'utf8')
-c = c.replace(/import \{ HandlerOptions \} from "\.\.\/common\/interfaces"/g, '')
-c = c.replace(/props: HandlerOptions/g, 'props: any')
-writeFileSync('packages/design-editor/src/engine/core/controllers/Guidelines.ts', c)
